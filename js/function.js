f7ac4fe2a8ee50faba8fc6d3c02c109b93806ac3
高橋泰 <takahashiyutaka@takahashiyasushinoMacBook-ea.local>
2019-09-05 03:15:04 +0900

    first commit

---
+++ js/function.js
@@ -0,0 +1,11 @@
+$(function() {
+    $('#vegas').vegas({
+        slides: [
+            { src: 'img/iphone8IMGL7757.jpg' },
+            { src: 'img/tokyo0720IMGL6995.jpg' }
+        ],
+        transitionDuration: 4000,
+        delay: 10000, 
+        animationDuration: 40000,
+    });
+});
